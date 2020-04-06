40963a5d66ca8c649accab4bfe61570e461960b0
Jo Seong-cheol <josworks27@gmail.com>
2020-04-06 14:14:14 +0900

    Contact component 이펙트 적용

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
+import Zoom from 'react-reveal/Zoom';
 import data from '../../data';
 
 function Contact() {
   return (
     <div className="Contact-wrap">
       <h1>Contact</h1>
-      <div className="Contact-content">
-        <a
-          className="Contact-mail"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`mailto:${data.contactEmail}`}
-        >
-          {data.contactEmail}
-        </a>
-        <ul>
-          {data.social.map((link, index) => (
-            <li key={index}>
-              <a
-                className="Contact-social"
-                target="_blank"
-                rel="noopener noreferrer"
-                href={link.url}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Zoom>
+        <div className="Contact-content">
+          <a
+            className="Contact-mail"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`mailto:${data.contactEmail}`}
+          >
+            {data.contactEmail}
+          </a>
+          <ul>
+            {data.social.map((link, index) => (
+              <li key={index}>
+                <a
+                  className="Contact-social"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={link.url}
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Zoom>
 
       <div className="footer">
         <span role="img" aria-label="Rainbow">
           🌈
         </span>
-        Made by 조성철(IKU_JO)
+        Made by Jo Seong-cheol
       </div>
     </div>
   );
