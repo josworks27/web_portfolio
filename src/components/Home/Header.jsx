1359b5b10aa9b2f44a5331a41bf4d1a8564de614
Jo Seong-cheol <josworks27@gmail.com>
2020-04-14 13:16:15 +0900

    Project content 줄 간격 변경

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import Flash from 'react-reveal/Flash';
+// import Flash from 'react-reveal/Flash';
 
 function Header() {
   return (
     <div>
       <h1 className="Header-greeting">
-        <Flash>
-          <div>
-            <span>Jo Seong-cheol's </span>
-            <span>Portfolio</span>
-            <span>.</span>
-          </div>
-        </Flash>
+        {/* <Flash> */}
+        <div>
+          <span>Jo Seong-cheol's </span>
+          <span>Portfolio</span>
+          <span>.</span>
+        </div>
+        {/* </Flash> */}
       </h1>
     </div>
   );
