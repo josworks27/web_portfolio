6d52a97da881e43f97f53ead1c08795b78a539c7
Jo Seong-cheol <josworks27@gmail.com>
2020-04-24 16:20:57 +0900

    Updates

---
+++ precache-manifest.b23d768ba58c3e7884f07638f529a835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896a3e737509b6baea2429017e9aa604",
+    "url": "/web_portfolio/index.html"
+  },
+  {
+    "revision": "41555f2e4b17dc37ed6e",
+    "url": "/web_portfolio/static/css/main.2a48421b.chunk.css"
+  },
+  {
+    "revision": "fd125a9ee9782dacd1f4",
+    "url": "/web_portfolio/static/js/2.1f5c21fd.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/web_portfolio/static/js/2.1f5c21fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41555f2e4b17dc37ed6e",
+    "url": "/web_portfolio/static/js/main.2b59077c.chunk.js"
+  },
+  {
+    "revision": "75dc12f78efdc4966797",
+    "url": "/web_portfolio/static/js/runtime-main.f699bc28.js"
+  }
+]);
